366da9443a35879059af5b2e604e472837518032
firdous alam <firdous.alam2058@gmail.com>
2024-05-05 12:24:48 +0530

    caampany

--- src/components/Service.js
+++ src/components/Service.js
@@ -15,7 +15,7 @@ export default function Service() {
                     <img src={thumbone} alt="" />
                 </div>
                    
-                    <p><strong>Manpower Supply</strong> Al Qannaatii-BKC Group of Companies provides various categories of skilled and unskilled labor to major establishments focusing on Contracting, Industries, Construction and Refineries across United Arab Emirates. We work with dedication and passion to provide uninterrupted service to our clients to fulfill their goals in a timely manner.
+                    <p><strong>Manpower Supply</strong> FALAH ALKHAIR TECHNICAL SERVICES CO Group of Companies provides various categories of skilled and unskilled labor to major establishments focusing on Contracting, Industries, Construction and Refineries across United Arab Emirates. We work with dedication and passion to provide uninterrupted service to our clients to fulfill their goals in a timely manner.
 
                 We provide almost all categories of workers as given below:
                     <ul>
@@ -47,7 +47,7 @@ To us HSE excellence means working towards an incident free workplace, finding b
             <h2>Conditions We Treat</h2>
             <div className="crow">
                 <div className="col-l">
-                    <p>Al Qannaatii-BKC Group of Companies was established in the year 2002 with its headquarters in Sharjah and subsidiary offices in Dubai and Ajman .Over the years the company has evolved into one of the leading Manpower Supply & Outsourcing Group in the United Arab Emirates. </p>
+                    <p>FALAH ALKHAIR TECHNICAL SERVICES CO Group of Companies was established in the year 2002 with its headquarters in Sharjah and subsidiary offices in Dubai and Ajman .Over the years the company has evolved into one of the leading Manpower Supply & Outsourcing Group in the United Arab Emirates. </p>
                 </div>
                 <div className="col-r">
                     <p>We believe in taking a whole-person approach to care, which means that we'll work with you to identify the underlying causes of your pain or discomfort.</p>
